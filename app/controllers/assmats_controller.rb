5ec7d09e16307de981f241fde6345b1892ad42a6
Bastien Hubervic-Renard <bastienhr+github@gmail.com>
2021-04-07 21:10:33 +0200

    init routes and Assmats controller

---
+++ app/controllers/assmats_controller.rb
@@ -0,0 +1,2 @@
+class AssmatsController < ApplicationController
+end
---
+++ test/controllers/assmats_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class AssmatsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
